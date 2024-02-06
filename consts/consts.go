2ed7027eb3b45495a9decb17661ddbd8220f67c5
manvisinha <131350826+manvisinha@users.noreply.github.com>
2024-02-06 15:35:44 +0530

    consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Red Cherry tint"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Cherry"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "CR"
 )
 
 var ID ids.ID
